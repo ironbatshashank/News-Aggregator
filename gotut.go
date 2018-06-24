e487ee0434ebe6cc873e4b5852a4464e50cfd67a
Shashank Srivastava <shashanksri65@gmail.com>
2018-06-24 15:46:17 +0530

    added location

--- gotut.go
+++ gotut.go
@@ -9,6 +9,10 @@ type Sitemapindex struct {
 	Locations []Location 'xml:"sitemap"'
 }
 
+type Location struct {
+	Loc string 'xml:"loc"'
+}
+
 func main() {
 	a_car := car{gas_pedal: 22341, 
 				brake_pedal: 0, 
