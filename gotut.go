4c7b6d742959febedfe4fa20395dd39a751712e4
Shashank Srivastava <shashanksri65@gmail.com>
2018-06-24 20:36:24 +0530

    sitemap data

--- gotut.go
+++ gotut.go
@@ -1,29 +1,14 @@
 package main
 
 import ("fmt"
-		"encoding/xml"
+//		"encoding/xml"
 		"io/ioutil"
 		"net/http")
 
-type Sitemapindex struct {
-	Locations []Location 'xml:"sitemap"'
-}
-
-type Location struct {
-	Loc string 'xml:"loc"'
-}
-
-func(L Location) String() string {
-	return fmt.Sprintf(1.Loc)
-}
-
 func main() {
-	resp, _ := http.Get("https://www.washintonpost.com/news-sitemap-index.xml")
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	var s Sitemapindex
-	xml.Unmarshal(bytes, &s)
-
-	for _, Location := range s.Locations {
-		fmt.Printf("\n%s", Location)
-	}
+	string_body := string(bytes)
+	fmt.Println(string_body)
+	resp.Body.Close()
 }
