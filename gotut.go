8ab8349d92ebdbd4da516cec33699a783f6eef91
Shashank Srivastava <shashanksri65@gmail.com>
2018-06-24 20:51:59 +0530

    sliced

--- gotut.go
+++ gotut.go
@@ -1,14 +1,29 @@
 package main
 
 import ("fmt"
-//		"encoding/xml"
+		"encoding/xml"
 		"io/ioutil"
 		"net/http")
+
+type SitemapIndex struct {
+	Locations []Location `xml:"sitemap"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
 
 func main() {
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	string_body := string(bytes)
-	fmt.Println(string_body)
 	resp.Body.Close()
+
+	var s SitemapIndex
+	xml.Unmarshal(bytes, &s)
+
+	fmt.Println(s.Locations)
 }
