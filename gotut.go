e5230a9c7909277814774c3afa02911678203c8c
Shashank Srivastava <shashanksri65@gmail.com>
2018-06-25 18:33:18 +0530

    Extracted from sitemap

--- gotut.go
+++ gotut.go
@@ -9,12 +9,23 @@ type SitemapIndex struct {
 	Locations []string `xml:"sitemap>loc"`
 }
 
+type News struct {
+	Titles []string `xml:"url>news>title"`
+	Keywords []string `xml:"url>news>Keywords"`
+	Locations []string  `xml:"url>loc"`
+}
+
 func main() {
+	var s SitemapIndex
+	var n News
+
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	var s SitemapIndex
 	xml.Unmarshal(bytes, &s)
+
 	for _, Location := range s.Locations {
-		fmt.Printf("\n%s", Location)
+		resp, _ := http.Get(Locations)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes, &n)
 	}
 }
