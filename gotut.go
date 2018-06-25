312626c05af14a96018d28ede7a0873c139a3cd3
Shashank Srivastava <shashanksri65@gmail.com>
2018-06-25 19:39:45 +0530

    Removed the direct code

--- gotut.go
+++ gotut.go
@@ -15,33 +15,13 @@ func newsAggHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(t.Execute(w, p))
 }
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Go is neat</h1>")
+}
+
+
 func main() {
-	var s SitemapIndex
-	var n News
-	news_map := make(map[string]NewsMap)
-
-	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
-	bytes, _ := ioutil.ReadAll(resp.Body)
-	xml.Unmarshal(bytes, &s)
-
-	for _, Location := range s.Locations {
-		resp, _ := http.Get(Location)
-
-		fmt.Println("\n\n\n",idx)
-		fmt.Println("\n",data.Keyword)
-
-		bytes, _ := ioutil.ReadAll(resp.Body)
-
-		fmt.Println("\n\n\n",idx)
-		fmt.Println("\n",data.Keyword)
-		xml.Unmarshal(bytes, &n)
-		for idx, _ := range n.Titles{
-			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
-		}
-	}
-	for idx, data := range news_map {
-		fmt.Println("\n\n\n",idx)
-		fmt.Println("\n",data.Keyword)
-		fmt.Println("\n",data.Location)
-	}
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/agg/", newsAggHandler)
+	http.ListenAndServe(":8000", nil)
 }
